578f2cf9a82c285820feaad49a7c5ec32b19cb90
Mustafa Yalciner <mustafa.yalciner@tu-dortmund.de>
2021-06-03 15:53:54 +0200

    ilk commit.

---
+++ Main.py
@@ -0,0 +1,10 @@
+from NotMain import NotMain
+
+
+def main():
+    print("Merhabalar!")
+
+if __name__ == "__main__":
+    main()
+    notmain = NotMain()
+    notmain.asdf()
---
+++ NotMain.py
@@ -0,0 +1,11 @@
+class NotMain():
+    number = 10
+
+    def __init__(self):
+        print('HIIII')
+        print(self.number)
+        self.number = self.number +1
+
+    def asdf(self):
+        print(self.number)
+
