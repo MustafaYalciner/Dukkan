961eb3b971dccfda37411cf60a4b0573cdae4c48
Mustafa Yalciner <mustafa.yalciner@tu-dortmund.de>
2021-06-10 16:41:39 +0200

    Gereksiz sinifi sil.

--- Main.py
+++ Main.py
@@ -1,10 +1,5 @@
-from NotMain import NotMain
 
 
-def main():
-    print("Merhabalar!")
 
 if __name__ == "__main__":
-    main()
-    notmain = NotMain()
-    notmain.asdf()
+    print("Merhabalar!")
