39f738d97dd66fb7dad4a557a94cedb3eb9afbfd
fatihsenguel <prod.fati@gail.comgit config --global user.email prod.fati@gmail.com>
2021-07-02 22:22:46 +0200

    SELAM

--- Main.py
+++ Main.py
@@ -21,6 +21,82 @@ Dukkanlar = [Dukkan("Pasa Doner",True,Lokasyon(11,14)),Dukkan("Firin Sanati",Fal
 
 siparisler=[Siparis("ismet","cetin",Lokasyon(11,11),"tavukdurum")]
 
+# ________ YENI _______________________________________________________________________
+
+dukkanlar_dict = {}
+dukkanlar_dict_class = {}
+
+
+@app.route('/todo/api/v1.0/dukkan/<dukkan_user>', methods=['POST'])
+def dukkan_login(dukkan_user):
+    # session.clear()
+    # location, availability
+    login_data = jsonpickle.encode(request.get_data())
+
+    session[dukkan_user] = []
+    dukkanlar_dict[dukkan_user] = []
+
+    # dukkanlar_dict_class[dukkan_user] = []
+
+    # aslinda session kurduk, ve dukkan offline olursa session de kaniyor. Ama daha tam emin olmadigimdan ve REST CLIENT sadece string yolladigindan
+    def boolean_converter(bool):
+        if bool == "1" or "True" or "true" or "TRUE":
+            return True
+        else:
+            return False
+
+    # class topla
+    yeni_dukkan_class = Dukkan(dukkan_user, boolean_converter(login_data[1]), Lokasyon(login_data[2][0], login_data[2][2]))
+
+    dukkanlar_dict_class[yeni_dukkan_class] = []
+
+    print(f"sessions: {session}, classes: {dukkanlar_dict_class}, storage: {dukkanlar_dict}")
+
+    return dukkanlar_dict, dukkanlar_dict_class
+
+
+@app.route('/todo/api/v1.0/dukkan/<dukkan_user>/siparis', methods=['PUT'])
+def dukkan_siparis(dukkan_user):
+    siparis_data = jsonpickle.decode(request.get_data())
+    print(siparis_data)
+    """
+    if dukkan_user in session:
+        print("hallo")
+    else:
+        print("bye")
+    """
+
+    if dukkan_user in dukkanlar_dict:
+        dukkanlar_dict[dukkan_user].append(siparis_data)
+        # dukkanlar_dict_class[Dukkan(dukkan_user, True)].append(Siparis(siparis_data[0], Lokasyon(siparis_data[1][0], siparis_data[1][2]), siparis_data[2]))
+    else:
+        pass
+
+    print(dukkanlar_dict)
+    print(dukkanlar_dict_class)
+
+    return dukkanlar_dict
+
+
+# REST CLIENTE BUNU YAZ:
+
+"""
+1. Method: POST - url: http://127.0.0.1:5000/todo/api/v1.0/dukkan/farketmezistediginiyaz
+                        body: ["farketmezistediginiyaz", "true", "1,2"]
+
+2. Method PUT - url    http://127.0.0.1:5000/todo/api/v1.0/dukkan/farketmezistediginiyaz/siparis
+                  body:     ["Fatih Senguel", "2,3", "Adana ve Cola"]
+
+
+sonra dukkan ismini degistir veya yeni siparis ver
+
+"""
+
+
+# ________________________________________________________________________________________________________________
+
+
+
 @app.route('/todo/api/v1.0/siparisler', methods=['PUT'])
 def update_siparisler():
     Siparis = jsonpickle.decode(request.get_data())
